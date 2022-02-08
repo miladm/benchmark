4e9175aa2648cab8f31a46d6527c230345cc9675
Xu Zhao <xzhao9@fb.com>
2022-02-08 15:10:13 -0500

    Enable fx2trt in argument.

--- torchbenchmark/util/extra_args.py
+++ torchbenchmark/util/extra_args.py
@@ -1,10 +1,12 @@
-import torch
 import argparse
-from typing import List, Tuple
+from typing import List
+
+from torchbenchmark.util.backends.fx2trt import enable_fx2trt
 
 # Dispatch arguments based on model type
 def parse_args(model: 'torchbenchmark.util.model.BenchmarkModel', extra_args: List[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument("--fx2trt", action='store_true', help="enable fx2trt")
     # by default, enable half precision for inference
     args = parser.parse_args(extra_args)
     args.device = model.device
@@ -12,13 +14,11 @@ def parse_args(model: 'torchbenchmark.util.model.BenchmarkModel', extra_args: Li
     # some models don't support train or eval tests, therefore they don't have these attributes
     args.train_bs = model.train_bs if hasattr(model, 'train_bs') else None
     args.eval_bs = model.eval_bs if hasattr(model, 'eval_bs') else None
-    # CUDA Graph is only supported for torchvision models
-    args.cudagraph = None
     return args
 
 def apply_args(model: 'torchbenchmark.util.model.BenchmarkModel', args: argparse.Namespace):
-   pass
-
-def enable_fp16(model: torch.nn.Module, example_input: Tuple[torch.tensor]) -> Tuple[torch.nn.Module, Tuple[torch.tensor]]:
-    return model.half(), (example_input[0].half(),)
-
+   if args.fx2trt:
+       assert args.device == 'cuda', "fx2trt is only available with CUDA."
+       assert args.eval_bs, "fx2trt only applies to CUDA eval tests."
+       assert not args.jit, "fx2trt with JIT is not available."
+       model.eval_model = enable_fx2trt(args.eval_bs, args.eval_fp16, model.eval_model, model.eval_example_inputs)
